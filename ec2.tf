a78c1e9bf9d6c395d70aa62f454f4fc1b4b013a5
yogesh <yogeshbodke2024@gmail.com>
2024-09-19 23:39:11 +0000

    EC2-Instance

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "XXXXX"
+  secret_key = "XXXXX"
+}
+resource "aws_instance" "Dev" {
+  ami           = "ami-XXXXX"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DEV-EC2"
+  }
+}
